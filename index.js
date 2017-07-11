cfbb025080c7605de3d94e732615539bd15107d8
Heather Arvidson <heather.arvidson@gmail.com>
2017-07-11 16:21:05 -0700

    marker functionality works for computers test

--- index.js
+++ index.js
@@ -1,82 +1,131 @@
+const $script = $('<script>');
+$script.attr('src', `https://maps.googleapis.com/maps/api/js?key=${googleMapsAPI}&callback=initMap`).attr('async');
+$('body').append($script);
+
 let markers = [];
+let map;
 
 function initMap() {
-     const seattle = {lat: 47.620828, lng: -122.332982};
-     const map = new google.maps.Map(document.getElementById('map'), {
-       zoom: 12,
-       center: seattle
-     });
+  const seattle = {
+    lat: 47.620828,
+    lng: -122.332982
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: seattle
+  });
+
+  $('#computers').click(function(e){
+    createComputerMarkers();
+  });
 
 
-     const infoWindow = new google.maps.InfoWindow({
+}
 
-     });
-// establishes map boundaries
-     let bounds = new google.maps.LatLngBounds();
+function createComputerMarkers(){
 
-// sample set of locations to mark
-   const threadcycleGoodwill = [
-    //  {title: '', location: {lat: , lng: }, address: ''},
-    {title: 'Goodwill Northgate', location: {lat: 47.704994, lng: -122.323118}, address: `10685 5th Ave NE, Seattle, WA, 98125`},
-    {title: 'Goodwill Greenwood', location: {lat: 47.701921, lng: -122.361035}, address: `10022 Holman Rd. NW, Seattle, WA, 98177`}
-   ]
 
-// loops through sample locations, adding a marker object to markers for each one
-   for (let i = 0; i < threadcycleGoodwill.length; i++) {
-     const position = threadcycleGoodwill[i].location;
-     const title = threadcycleGoodwill[i].title;
-     const marker = new google.maps.Marker({
-       map: map,
-       position: position,
-       title: title,
-       animation: google.maps.Animation.DROP,
-       id: i
-     });
-     markers.push(marker);
-// adjust map scope to fit the marker
-     bounds.extend(marker.position);
-// listen for click event on earch marker
-     marker.addListener('click', function() {
-       populateInfoWindow(this, infoWindow);
-     })
-// give location details in an infoWindow for selected marker
-     function populateInfoWindow(marker, infoWindow) {
-       if (infoWindow.marker != marker) {
-         infoWindow.marker = marker;
-         infoWindow.setContent(`<div>${marker.title}</div>`);
-         infoWindow.open(map, marker);
-         infoWindow.addListener('closeclick', function() {
-           infoWindow.setMarker(null);
-         });
-       }
-     }
-   }
-   map.fitBounds(bounds);
+  const marker = new google.maps.Marker({
+    position: {
+      lat: 47.620830,
+      lng: -122.332990
+    },
+    map: map
+  });
+  markers.push(marker);
+}
+
+//this thing has to be disassembled and put into mapInit
+function createMarkers() {
+  const infoWindow = new google.maps.InfoWindow({});
+
+  // establishes map boundaries
+  let bounds = new google.maps.LatLngBounds();
+
+  //call getResults
+
+  // mock-up--loops through sample locations, adding a marker object to markers for each one
+
+  for (let i = 0; i < threadcycle.length; i++) {
+    const position = threadcycle[i].location;
+    const title = threadcycle[i].title;
+    const marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
+    markers.push(marker);
+    // adjust map scope to fit the marker
+    bounds.extend(marker.position);
+    // listen for click event on earch marker
+    marker.addListener('click', function() {
+      populateInfoWindow(this, infoWindow);
+    });
+    // give location details in an infoWindow for selected marker
+    function populateInfoWindow(marker, infoWindow) {
+      if (infoWindow.marker != marker) {
+        infoWindow.marker = marker;
+        infoWindow.setContent(`<div>${marker.title}</div>`);
+        infoWindow.open(map, marker);
+        infoWindow.addListener('closeclick', function() {
+          //weird bug here--if you close the infoWindow you can't immediately click on it again to reopen
+          infoWindow.close();
+        });
+      }
+    }
+  }
+  map.fitBounds(bounds);
+}
+
+function adjustBounds() {
+
+}
+
+function addInfoWindows() {
+
 }
 
 // add event listeners to all the materials
+
 $('.categories').on('click', 'li', function(event) {
   // console.log($(this).attr('id'));
+
   const selectedMaterial = $(this).attr('id');
-  getMaterialHandled(selectedMaterial);
+  //if id=threadcycle, use hardcoded array as data source to map options
+  if (selectedMaterial === 'threadcycle') {
+    // threadcycle();
+    console.log("you picked threadcycle; you should get a toast telling you more about this program");
+    //if id=garbage, use a toast to notify
+  } else if (selectedMaterial === 'garbage') {
+    console.log("you'll get a toast telling you to throw that stuff in garbage");
+    //otherwise, use api call against county data to map options
+  } else {
+    getMaterialHandled(selectedMaterial);
+  }
 });
 
+// change id of selected item into a string that matches material_handled in county json
 function getMaterialHandled(selectedMaterial) {
-  let materialHandled = 'Paint';
+  console.log(nameMatches[selectedMaterial]);
+  let materialHandled = nameMatches[selectedMaterial];
   getResults(materialHandled);
 }
 
 function getResults(materialHandled) {
-  $.ajax( {
+  $.ajax({
     url: `https://data.kingcounty.gov/resource/tzui-ygc5.json?city=Seattle&material_handled=${materialHandled}`,
     dataType: 'json',
-  } ).done(function(data) {
+  }).done(function(data) {
     console.log(data);
-  })
+    return data;
+  }).fail(function() {
+    alert("There was an error. Please search again.")
+  });
 }
 
-// converts id of selected item into a string that matches material_handled in county json
-
-
-
-//use an object to run through different options re. what got clicked?
+//generate map markers from threadcycle object
+// function threadcycle() {
+//
+// }
---
+++ objects.js
@@ -0,0 +1,25 @@
+
+// keys are ids from index.html; values are names of material_handled in county JSON
+const nameMatches = {
+    clothing: "Clothing, Shoes, and Fabrics",
+    cameras: "Audio Video and Camera equipment",
+    av: "Audio Video and Camera equipment",
+    phones: "Cell Phones, Smart Phones, Mobile Devices",
+    computers: "Computers, Laptops, Tablets",
+    games: "Gaming Devices",
+    monitors: "Monitors",
+    tvs: "TVs",
+    printers: "Printers, Copiers, Fax Machines, Peripherals",
+    cartridges: "Inkjet Cartridges"
+
+};
+//
+// const threadcycle = {
+//
+// }
+
+const threadcycle = [
+ //  {title: '', location: {lat: , lng: }, address: ''},
+    {title: 'Goodwill Northgate', location: {lat: 47.704994, lng: -122.323118}, address: `10685 5th Ave NE, Seattle, WA, 98125`},
+    {title: 'Goodwill Greenwood', location: {lat: 47.701921, lng: -122.361035}, address: `10022 Holman Rd. NW, Seattle, WA, 98177`},
+];
