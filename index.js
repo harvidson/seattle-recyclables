c03db379c37e42713ba0354b1ff9a40d655bc2cd
Heather Arvidson <heather.arvidson@gmail.com>
2017-07-12 13:43:21 -0700

    searches get different colors

--- index.js
+++ index.js
@@ -1,94 +1,19 @@
-const $script = $('<script>');
-$script.attr('src', `https://maps.googleapis.com/maps/api/js?key=${googleMapsAPI}&callback=initMap`).attr('async');
-$('body').append($script);
+// const $script = $('<script>');
+// $script.attr('src', `https://maps.googleapis.com/maps/api/js?key=${googleMapsAPI}`);
+// $('body').append($script);
 
 let markers = [];
 let map;
 
-function initMap() {
-  const seattle = {
-    lat: 47.620828,
-    lng: -122.332982
-  };
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: seattle
-  });
+let searchID = 0;
+const iconColors = ['ff6f00', '00e676', '18ffff', 'ffea00']
+const defaultIcon = makeMarkerIcon('ff6f00');
 
-  const infoWindow = new google.maps.InfoWindow({});
+initMap();
 
-  // establishes map boundaries
-  let bounds = new google.maps.LatLngBounds();
-
-//test case for creating markers. event listener for selection has to go up here
-  $('#computers').click(function(e){
-    createComputerMarkers(bounds);
-  });
-  // mock-up--loops through sample locations, adding a marker object to markers for each one
-
-  // for (let i = 0; i < markers.length; i++) {
-  //   const position = markers[i].location;
-  //   const title = markers[i].title;
-  //   const marker = new google.maps.Marker({
-  //     map: map,
-  //     position: position,
-  //     title: title,
-  //     animation: google.maps.Animation.DROP,
-  //     id: i
-  //   });
-  //   markers.push(marker);
-
-}
-
-function createComputerMarkers(bounds){
-  const marker = new google.maps.Marker({
-    position: {
-      lat: 47.620830,
-      lng: -122.332990
-    },
-    map: map
-  });
-  markers.push(marker);
-  // adjust map scope to fit the marker
-  bounds.extend(marker.position);
-  map.fitBounds(bounds);
-
-  // listen for click event on earch marker
-  marker.addListener('click', function() {
-    populateInfoWindow(this, infoWindow);
-  });
-  // give location details in an infoWindow for selected marker
-  function populateInfoWindow(marker, infoWindow) {
-    if (infoWindow.marker != marker) {
-      infoWindow.marker = marker;
-      infoWindow.setContent(`<div>${marker.title}</div>`);
-      infoWindow.open(map, marker);
-      infoWindow.addListener('closeclick', function() {
-        //weird bug here--if you close the infoWindow you can't immediately click on it again to reopen
-        infoWindow.close();
-      });
-    }
-  }
-}
-
-//this thing has to be disassembled and put into mapInit
-function createMarkers() {
-
-}
-
-function adjustBounds() {
-
-}
-
-function addInfoWindows() {
-
-}
-
-// add event listeners to all the materials
-
+// add event listeners to all the materials in drop-down lists
 $('.categories').on('click', 'li', function(event) {
   // console.log($(this).attr('id'));
-
   const selectedMaterial = $(this).attr('id');
   //if id=threadcycle, use hardcoded array as data source to map options
   if (selectedMaterial === 'threadcycle') {
@@ -103,6 +28,76 @@ $('.categories').on('click', 'li', function(event) {
   }
 });
 
+function initMap() {
+  const seattle = {
+    lat: 47.620828,
+    lng: -122.332982
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: seattle
+  });
+}
+
+function createMarkers(data) {
+  const infoWindow = new google.maps.InfoWindow({});
+  let bounds = new google.maps.LatLngBounds();
+
+//loop through data and
+  for (let i = 0; i < data.length; i++) {
+    const position = {
+      lat: data[i].geolocation.coordinates[1],
+      lng: data[i].geolocation.coordinates[0]
+    };
+    const title = data[i].provider_name;
+    const marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      icon: makeMarkerIcon(iconColors[searchID]),
+      id: i
+    });
+    markers.push(marker);
+    bounds.extend(marker.position);
+
+    // listen for click event on each marker
+    marker.addListener('click', function() {
+      populateInfoWindow(this, infoWindow);
+    });
+
+  }
+  map.fitBounds(bounds);
+  console.log(markers);
+  searchID++;
+}
+
+
+// make an array of marker colors; add this-- createMarkerIcon(markerColors[i])--to event listener on click
+function makeMarkerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+  return markerImage;
+}
+
+//add to this so that it's got more info: address, hours, learn more, save, etc.
+function populateInfoWindow(marker, infoWindow) {
+  if (infoWindow.marker != marker) {
+    infoWindow.marker = marker;
+    infoWindow.setContent(`<div>${marker.title}</div>`);
+    infoWindow.open(map, marker);
+    infoWindow.addListener('closeclick', function() {
+      //weird bug here--if you close the infoWindow you can't immediately click on it again to reopen
+      infoWindow.close();
+    });
+  }
+}
+
 // change id of selected item into a string that matches material_handled in county json
 function getMaterialHandled(selectedMaterial) {
   console.log(nameMatches[selectedMaterial]);
@@ -115,14 +110,10 @@ function getResults(materialHandled) {
     url: `https://data.kingcounty.gov/resource/tzui-ygc5.json?city=Seattle&material_handled=${materialHandled}`,
     dataType: 'json',
   }).done(function(data) {
-    console.log(data);
-    return data;
+    //i think line below will become createMarkers(data);
+    console.log("here's the data from KC: ", data);
+    createMarkers(data);
   }).fail(function() {
     alert("There was an error. Please search again.")
   });
 }
-
-//generate map markers from threadcycle object
-// function threadcycle() {
-//
-// }
